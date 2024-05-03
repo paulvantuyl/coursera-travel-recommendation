a428e11853b411a784feeabcd4eda66aa0e6602f
Paul <paul.vantuyl@keap.com>
2024-05-03 08:01:41 -0700

    Draft of search functionality

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -1,3 +1,44 @@
+const btnSearch = document.getElementById('btnSearch');
+const clearSearch = document.getElementById('clearSearch');
+const resultDiv = document.getElementById('searchResults');
+
+function resetForm() {
+	document.getElementById('travelSearch').value = '';
+    resultDiv.innerHTML = '';
+    resultDiv.classList.remove('visible');
+}
+
+function searchResults() {
+	const input = document.getElementById('travelSearch').value.toLowerCase();
+	resultDiv.innerHTML = '';
+
+	fetch('./travel_recommendation_api.json')
+		.then(response => response.json())
+		.then(data => {
+			const country = data.countries.find(item => item.name.toLowerCase() === input);
+
+			if (country) {
+                resultDiv.classList.add('visible');
+                resultDiv.innerHTML = `<h1>Results for ${country.name}</h1>`
+
+                data.countries.forEach(function(country) {
+                    resultDiv.innerHTML += `<img src="${country.imgUrl}" />`;
+                    resultDiv.innerHTML += `<h3>${country.name}</h3>`;
+                    resultDiv.innerHTML += `<p>${country.description}</p>`;
+                });
+			} else {
+				resultDiv.innerHTML = `Sorry, that country isn't in our list of destinations.`;
+			}
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+			resultDiv.innerHTML = 'An error occurred while fetching data.';
+		});
+}
+
+btnSearch.addEventListener('click', searchResults);
+clearSearch.addEventListener('click', resetForm);
+
 function thankyou() {
     alert('Thank you for contacting us!');
 }
