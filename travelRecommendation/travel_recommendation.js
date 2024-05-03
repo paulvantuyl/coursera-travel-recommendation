b6290ad1b94f53ae35d11b5186cc21cd20a08eae
Paul <paul.vantuyl@keap.com>
2024-05-03 11:48:12 -0700

    Create a way to run query agains keywords

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -12,23 +12,94 @@ function searchResults() {
 	const input = document.getElementById('travelSearch').value.toLowerCase();
 	resultDiv.innerHTML = '';
 
+	console.log("User searched for", input);
+
 	fetch('./travel_recommendation_api.json')
 		.then(response => response.json())
-		.then(data => {
+		.then(data => { 
 			const country = data.countries.find(item => item.name.toLowerCase() === input);
+			const temple = data.temples.find(item => item.name.toLowerCase() === input);
+			const beach = data.beaches.find(item => item.name.toLowerCase() === input);
 
-			if (country) {
-                resultDiv.classList.add('visible');
-                resultDiv.innerHTML = `<h1>Results for ${country.name}</h1>`
+			const keywords = [
+				"country", 
+				"countries", 
+				"australia",
+				"japan",
+				"cambodia",
+				"india",
+				"french polynesia",
+				"beach",
+				"beaches",
+				"temple",
+				"temples",
+			];
+			const found = keywords.find((keyword) => keyword === input);
+			console.log("The item found was", found);
+			
+			// while (keywords == input) {
+			// 	if (country) {
+			// 		resultDiv.classList.add('visible');
+			// 		resultDiv.innerHTML = `<h1>Results for ${country.name}</h1>`;
+			// 	} else if (temple) {
+			// 		resultDiv.classList.add('visible');
+			// 		resultDiv.innerHTML = `<h1>Results for ${temple.name}</h1>`;
+			// 	} else if (beach) {
+			// 		resultDiv.classList.add('visible');
+			// 		resultDiv.innerHTML = `<h1>Results for ${beach.name}</h1>`;
+			// 	} else {
+			// 		resultDiv.classList.add('visible');
+			// 		resultDiv.innerHTML = `Sorry, that isn't in our list of destinations.`;
+			// 		console.log('The user query of ' + input + " doesn't match any keywords.");
+			// 	}
+			// };
 
-                data.countries.forEach(function(country) {
-                    resultDiv.innerHTML += `<img src="${country.imgUrl}" />`;
-                    resultDiv.innerHTML += `<h3>${country.name}</h3>`;
-                    resultDiv.innerHTML += `<p>${country.description}</p>`;
-                });
-			} else {
-				resultDiv.innerHTML = `Sorry, that country isn't in our list of destinations.`;
-			}
+			// const match = keywords.forEach(function (keyword) {
+			// 	if (keyword == input) {
+			// 		if (country) {
+            //     		resultDiv.classList.add('visible');
+            //     		resultDiv.innerHTML = `<h1>Results for ${country.name}</h1>`
+
+			// 		} else if (temple) {
+			// 			resultDiv.classList.add('visible');
+			// 			resultDiv.innerHTML = `<h1>Results for ${temple.name}</h1>`;
+
+			// 		} else if (beach) {
+			// 			resultDiv.classList.add('visible');
+			// 			resultDiv.innerHTML = `<h1>Results for ${beach.name}</h1>`;
+			// 		};
+
+			// 		console.log("The keyword " + keyword + " matched to the search query of " + input + ".");
+
+			// 	} else {
+			// 		resultDiv.classList.add('visible');
+			// 		resultDiv.innerHTML = `Sorry, that isn't in our list of destinations.`;
+			// 		console.log("The user query of " + input + " doesn't match any keywords.")
+			// 	}
+			// });
+
+			// if (country) {
+            //     resultDiv.classList.add('visible');
+            //     resultDiv.innerHTML = `<h1>Results for ${country.name}</h1>`
+
+            //     country.cities.forEach(function(city) {
+            //         resultDiv.innerHTML += `<img src=".${city.imageUrl}" />`;
+            //         resultDiv.innerHTML += `<h3>${city.name}</h3>`;
+            //         resultDiv.innerHTML += `<p>${city.description}</p>`;
+            //     });
+
+			// } else if (temple) {
+			// 	resultDiv.classList.add('visible');
+			// 	resultDiv.innerHTML = `<h1>Results for ${temple.name}</h1>`;
+
+			// } else if (beach) {
+			// 	resultDiv.classList.add('visible');
+			// 	resultDiv.innerHTML = `<h1>Results for ${beach.name}</h1>`;
+				
+			// } else {
+			// 	resultDiv.classList.add('visible');
+			// 	resultDiv.innerHTML = `Sorry, that isn't in our list of destinations.`;
+			// }
 		})
 		.catch((error) => {
 			console.error('Error:', error);
